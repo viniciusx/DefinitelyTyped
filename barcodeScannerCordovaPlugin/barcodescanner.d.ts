b42b58a1c7b9770b44544824a43de0a3936fcbdb
viniciusx <medeirosvx@gmail.com>
2015-02-12 22:50:00 -0200

    declaration for the barcode plugin
    Work in progress, but still usable.
    Declaration for the cordova barcode plugin.
    the original JS plugin can be found here:
    https://github.com/wildabeast/BarcodeScanner.

---
+++ barcodeScannerCordovaPlugin/barcodescanner.d.ts
@@ -0,0 +1,32 @@
+﻿interface ResultInterface {
+    text: string;
+    format: string;
+    cancelled: boolean;
+}
+interface BarcodeEncode {
+    type: Encode;
+    data: any;
+    success: (success: any) => void;
+    fail: (error: any) => void;
+}
+interface EncodeOptions {
+    TEXT_TYPE: string;
+    EMAIL_TYPE: string;
+    PHONE_TYPE: string;
+    SMS_TYPE: string;
+}
+interface Encode {
+    Encode: EncodeOptions;
+    //cordova.plugins.barcodeScanner.Encode.TEXT_TYPE
+}
+interface BarCodeInterface {
+    scan(success: (data: ResultInterface) => void, error: (data: string) => void): void;
+    encode(type: string, data: any, success: (success: any) => void, fail: (error: any) => void): void;
+    Encode: EncodeOptions;
+}
+
+declare module "barcodescanner" {
+    export = barcodeScanner;
+}
+
+declare var barcodeScanner: BarCodeInterface;  
